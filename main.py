eb9ab75b9c8d453c6c3ad7aa1024c2b3fd8ff3be
KacperBober <kacperbober112@gmail.com>
2020-12-29 18:01:16 +0100

    opened image with simple processing

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cv2 as cv
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+
+    original = cv.imread("images/org.jpg")
+    edited = cv.imread("images/edited.jpg")
+
+    grinch = cv.absdiff(original, edited)
+    grey_grinch = cv.cvtColor(grinch, cv.COLOR_BGR2GRAY)
+
+    cv.imshow("Output", grey_grinch)
+    cv.waitKey(0)
+
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
